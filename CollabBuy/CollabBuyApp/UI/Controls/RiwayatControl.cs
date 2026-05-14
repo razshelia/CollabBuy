f6c8f2c7252797218a41f78bc67f9a0b320a0ad7
razshelia <rachel88aja@gmail.com>
2026-05-14 10:29:41 +0700

    1

---
+++ CollabBuy/CollabBuyApp/UI/Controls/RiwayatControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using CollabBuy.CollabBuyApp.Services;
+using CollabBuy.CollabBuyApp.Helpers;
+
+namespace CollabBuy.CollabBuyApp.UI.Controls
+{
+    public partial class RiwayatControl : UserControl
+    {
+        private CheckoutService checkoutService;
+        private int idUser;
+
+        public RiwayatControl(int idUser)
+        {
+            InitializeComponent();
+            this.idUser = idUser;
+            this.checkoutService = new CheckoutService();
+            this.MuatRiwayat();
+        }
+
+        private void MuatRiwayat()
+        {
+            // Pastikan CheckoutService memiliki method untuk ambil data per user
+            var data = this.checkoutService.AmbilRiwayatUser(this.idUser);
+            this.dgvRiwayat.DataSource = data;
+
+            if (data == null || data.Count == 0)
+                UXHelper.TampilkanSukses("Belum ada riwayat pesanan nih, Bestie! Yuk jajan dulu. ✨");
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            this.MuatRiwayat();
+        }
+    }
+}
